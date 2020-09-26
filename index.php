a2974f4ffe2ad26284e10b8557532b797ce0bc14
macbaze <dav.taras@gmail.com>
2020-09-26 14:25:57 +0300

    Добавил выбор цвета, лэйблы для инпутов. переписал некоторые функции

--- index.php
+++ index.php
@@ -9,15 +9,29 @@
 </head>
 <body>
 <section id="mainSection">
-	<span>
-		Это генератор простых изображений, которые удобно печатать и сканировать камерой смартфона из банковских приложений. Это может пригодиться, если вы хотите принимать переводы на карту, но по каким-либо причинам не желаете раскрывать номер телефона, привязанный к карте. Удобства такого способа очевидны: вам не нужна физическая карта (срок действия карты и фамилия остаётся в тайне), это быстрее чем вводить номер карты вручную (вероятность ошибки минимальная).
-	</span>
-	<form>
-		<input type="text" v-model="cardNumber" v-on:input="drawRect" maxlength="16"/>
-		<input type="text" v-model="cardHolderName" v-on:input="drawRect" maxlength="20"/>
-	</form>
-	<canvas id="cardImage" width="856" height="540"></canvas>
-	<button>Сохранить как..</button>
+  <span>
+    Это генератор простых изображений, которые удобно печатать и сканировать камерой смартфона из банковских приложений. Это может пригодиться, если вы хотите принимать переводы на карту, но по каким-либо причинам не желаете раскрывать номер телефона, привязанный к карте. Удобства такого способа очевидны: вам не нужна физическая карта (срок действия карты и фамилия остаётся в тайне), это быстрее чем вводить номер карты вручную (вероятность ошибки минимальная).
+  </span>
+  <form>
+    <div>
+      <label for="cardNumber">Номер карты (16 цифр):</label>
+      <input id="cardNumber" v-model.number="cardNumber" v-on:input="drawText" maxlength="16"/>
+      <label for="numberColor">Цвет:</label>
+      <input id="numberColor" type="color" name="number" v-model.lazy="colors.number" v-on:input="event => { colorChanged(event); }">
+    </div>
+    <div>
+      <label for="cardNumber">Имя держателя:</label>
+      <input id="cardNumber" v-model="cardHolderName" v-on:input="drawText" maxlength="20"/>
+      <label for="holderColor">Цвет:</label>
+      <input id="holderColor" type="color" name="holder" v-model.lazy="colors.holder" v-on:input="event => { colorChanged(event); }">
+    </div>
+    <div>
+      <label for="cardColor">Цвет фона:</label>
+      <input id="cardColor" type="color" name="card" v-model.lazy="colors.card" v-on:input="event => { colorChanged(event); }">
+    </div>
+  </form>
+  <canvas id="cardImage" width="856" height="540" :download="imageName"></canvas>
+  <a v-if="linkAvailable" :download="imageName" :key="cardNumber+cardHolderName" :href="imageLink()">Сохранить как..</a>
 </section>
 <script src="scripts/common.js"></script>
 </body>
--- scripts/common.js
+++ scripts/common.js
@@ -8,47 +8,75 @@ function formatNumber(value) {
 var app = new Vue({
   el: '#mainSection',
   data: {
-    vueCanvas: null,
+    cContext: null,
+    cHTMLElem: null,
     cardNumber: "1234567890123456",
     cardHolderName: "Имя Ф.",
-    cardColor: "#00000099",
-    numberColor: "#FFFFFFFF",
-    holderNameColor: "#FFFFFFFF"
+    colors: {number: "#00AA00", holder: "#FFFFFF", card: "#000000", throttled: "#FF0000"},
+    throttling: {timer: 0, state: false}
   },
   mounted() {
     let card_font = new FontFace('Credit Card', 'url(CREDC___.ttf)');
     card_font.load().then((font) => {
       document.fonts.add(font);
-      let c = document.getElementById('cardImage');
-      let ctx = c.getContext('2d');
-      this.vueCanvas = ctx;
-      this.drawRect();
-      //console.log('Font loaded');
+      this.cHTMLElem = document.getElementById('cardImage');
+      this.cContext = this.cHTMLElem.getContext('2d');
+      this.drawText();
     });
   },
   methods: {
-    drawRect() {
-      this.vueCanvas.clearRect(0, 0, 856, 540);
+    drawRect: function () {
+      this.cContext.clearRect(0, 0, 856, 540);
 
-      this.vueCanvas.lineJoin = "round";
-      this.vueCanvas.lineWidth = 60;
-      this.vueCanvas.beginPath();
-      this.vueCanvas.rect(30, 30, 796, 480);
-      this.vueCanvas.strokeStyle = this.cardColor;
-      this.vueCanvas.fillStyle = this.cardColor;
-      this.vueCanvas.fillRect(60, 60, 736, 420);
-      this.vueCanvas.stroke();
+      this.cContext.lineJoin = "round";
+      this.cContext.lineWidth = 60;
+      this.cContext.beginPath();
+      this.cContext.rect(30, 30, 796, 480);
+      this.cContext.strokeStyle = this.colors.card;
+      this.cContext.fillStyle = this.colors.card;
+      this.cContext.fillRect(60, 60, 736, 420);
+      this.cContext.stroke();
+    },
+    drawText: function () {
+      if (this.cContext) {
+        this.drawRect();
 
+        this.cContext.font = "46px Credit Card";
+        this.cContext.fillStyle = this.colors.number;
+        this.cContext.textAlign = "start";
+        this.cContext.textBaseline = "top";
+        this.cContext.fillText(formatNumber(this.cardNumber), 83, 311);
 
-      this.vueCanvas.font = "46px Credit Card";
-      this.vueCanvas.fillStyle = this.numberColor;
-      this.vueCanvas.textAlign = "start";
-      this.vueCanvas.textBaseline = "top";
-      this.vueCanvas.fillText(formatNumber(this.cardNumber), 83, 311);
-
-      this.vueCanvas.font = "bold 40px Monospace";
-      this.vueCanvas.fillStyle = this.holderNameColor;
-      this.vueCanvas.fillText(uppercaseHolderName(this.cardHolderName), 56, 440);
+        this.cContext.font = "bold 40px Monospace";
+        this.cContext.fillStyle = this.colors.holder;
+        this.cContext.fillText(uppercaseHolderName(this.cardHolderName), 56, 440);
+      }
+    },
+    colorChanged: function (pickerEvent) {
+      this.colors.throttled = pickerEvent.target.value;
+      if (!this.throttling.state) {
+        //alert('nt'+pickerEvent.target.value)
+        this.throttling.state = true;
+        this.drawText();
+        this.throttling.timer = setTimeout(function () {
+            app.throttling.state = false;
+            app.colors[pickerEvent.target.name] = app.colors.throttled;
+            app.drawText();
+          }, 200);
+      }
+    },
+    imageLink: function () {
+      if (this.cContext) {
+        return this.cHTMLElem.toDataURL();
+      }
+    }
+  },
+  computed: {
+    linkAvailable: function () {
+      return this.cardNumber.length > 4;
+    },
+    imageName: function() {
+      return this.cardNumber.slice(-4) + '.png';
     }
   }
 });
